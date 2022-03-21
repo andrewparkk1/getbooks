24553d6e59583ecf1b38086f934dea95a1ec7c77
Andrew Park <andysampark@gmail.com>
2022-03-20 23:21:51 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GetBooks</title>
+    <link href="https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="next">
+        <header class="sticky py-6 bg-gray-200 w-full">
+            <div class="flex flex-row justify-between w-9/12 mx-auto">
+                <a href="index.html" class="text-xl hover:text-blue-500">GetBooks</a>
+                <a href="login.html " class="text-lg hover:text-blue-500">Log-In</a>
+            </div>
+        </header>
+
+        <div class="flex flex-col px-12 flex-grow text-center justify-center items-center">
+            <div class="text-center mx-auto w-6/12">
+                <h1 class=" pb-10 text-2xl">Sign-Up</h1>
+                <div class="flex flex-row justify-between">
+                    <a href="student.html ">
+                        <div class="md:w-24 md:h-24 lg:w-36 lg:h-24 text-center rounded-[50%] bg-gray-700 flex items-center hover:bg-gray-400">
+                            <p class="w-full text-center text-white">Student</p>
+                        </div>
+                    </a>
+                    <a href="professor.html ">
+                        <div class="md:w-24 md:h-24 lg:w-36 lg:h-24 text-center rounded-[50%] bg-gray-700 flex items-center hover:bg-gray-400">
+                            <p class="w-full text-center text-white">Professor</p>
+                        </div>
+                    </a>
+                    <a href="school.html ">
+                        <div class="md:w-24 md:h-24 lg:w-36 lg:h-24 text-center rounded-[50%] bg-gray-700 flex items-center hover:bg-gray-400">
+                            <p class="w-full text-center text-white">School</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <!-- <div class="flex flex-row items-center ">
+                <img src="images/bunch.jpg " class="w-[32rem] h-auto mx-20 ">
+                <h1 class="mx-20 text-center text-2xl ">Easy way to get books for school</h1>
+            </div> -->
+        </div>
+
+        <footer class="bg-gray-600 text-white w-full">
+            <div class="flex flex-row justify-between py-8 w-7/12 mx-auto ">
+                <div class="flex flex-col">
+                    <h1>About</h1>
+                    <br>
+                    <a>Blog</a>
+                    <a>Our Team</a>
+                </div>
+                <div class="flex flex-col ">
+                    <h1>Download</h1>
+                    <br>
+                    <a>App</a>
+                    <a>Website</a>
+                </div>
+                <div class="flex flex-col ">
+                    <h1>Sales</h1>
+                    <br>
+                    <a>Universities</a>
+                    <a>High School</a>
+                    <a>Middle School</a>
+                    <a>Elementary School</a>
+                </div>
+                <div class="flex flex-col ">
+                    <h1>Support</h1>
+                    <br>
+                    <a>Test GetBooks</a>
+                    <a>Account</a>
+                    <a>Support Center</a>
+                    <a>Feedback</a>
+                    <a>Contact Us</a>
+                    <a>Accessibility</a>
+                    <a>Privacy, Security, Legal</a>
+                </div>
+
+            </div>
+
+
+        </footer>
+
+
+
+    </div>
+
+
+</body>
+
+</html>
